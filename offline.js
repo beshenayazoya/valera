506874d66ba62b03efe5aa029f57416487a32512
beshenayazoya <dnsnf06@gmail.com>
2026-06-04 18:29:11 +0600

    Add files via upload

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1780560664,
+	"version": 1780572150,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -15,6 +15,8 @@
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
 		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
